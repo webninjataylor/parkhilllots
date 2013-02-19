9ec46bcd9b1594032b14683d25b183451b789986
Taylor Johnson <webninjataylor@gmail.com>
2013-02-19 18:33:42 -0500

    CodeKit and Fabric integration

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+//@codekit-prepend "jquery-1.9.0.min.js";
+//@codekit-prepend "modernizr.min.js";
+//@codekit-prepend "jquery.dataTables.min.js";
+//@codekit-prepend "wnt.js";
+
+
+
 /******** GOOGLE ANALYTICS ********/
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-31306164-2']);
+_gaq.push(['_setAccount', 'UA-38473382-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+<<<<<<< .merge_file_suxiFX
+})();
+=======
 })();
 
 /******** LOCK WINDOW ********/
@@ -92,3 +102,4 @@ if($('#prices').length != 0){   //If the pricing table exists...
         });*/
     });
 };
+>>>>>>> .merge_file_wWGCUX
