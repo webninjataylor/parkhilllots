4e2ed5334202bbd2224b894add11f9db78aaa79e
Taylor Johnson <webninjataylor@gmail.com>
2013-02-19 20:42:06 -0500

    Fixed JS from bad merge

--- js/main.js
+++ js/main.js
@@ -14,9 +14,6 @@ _gaq.push(['_trackPageview']);
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-<<<<<<< .merge_file_suxiFX
-})();
-=======
 })();
 
 /******** LOCK WINDOW ********/
@@ -102,4 +99,3 @@ if($('#prices').length != 0){   //If the pricing table exists...
         });*/
     });
 };
->>>>>>> .merge_file_wWGCUX
