ba055c8710671a99086fdcdfcb1a2f2bc187a3d6
Taylor Johnson <webninjataylor@gmail.com>
2013-03-20 01:14:32 -0400

    New plats

--- plats.php
+++ plats.php
@@ -14,6 +14,16 @@
             <img src="images/plat.gif" alt="Plats" class="plats">
             <p><a href="docs/plats-details.pdf" target="_blank">View Detailed Plats</a></p>
             <ul id="plats">
+                <li><a href="docs/lot_3.pdf" target="_blank">Lot 3</a></li>
+                <li><a href="docs/lot_4.pdf" target="_blank">Lot 4</a></li>
+                <li><a href="docs/lot_5.pdf" target="_blank">Lot 5</a></li>
+                <li><a href="docs/lot_6.pdf" target="_blank">Lot 6</a></li>
+                <li><a href="docs/lot_7.pdf" target="_blank">Lot 7</a></li>
+                <li><a href="docs/lot_8.pdf" target="_blank">Lot 8</a></li>
+                <li><a href="docs/lot_9.pdf" target="_blank">Lot 9</a></li>
+                <li><a href="docs/lot_10.pdf" target="_blank">Lot 10</a></li>
+                <li><a href="docs/lot_11.pdf" target="_blank">Lot 11</a></li>
+                <li><a href="docs/lot_12.pdf" target="_blank">Lot 12</a></li>
                 <li><a href="docs/lot_13.pdf" target="_blank">Lot 13</a></li>
                 <li><a href="docs/lot_14.pdf" target="_blank">Lot 14</a></li>
                 <li><a href="docs/lot_15.pdf" target="_blank">Lot 15</a></li>
